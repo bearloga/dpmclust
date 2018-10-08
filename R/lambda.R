79fe748db6eb03ecbe66f9bba351ca647136d2bc
Mikhail Popov <michaelupopov@gmail.com>
2018-10-08 12:12:05 -0400

    Start implementing lambda means

---
+++ R/lambda.R
@@ -0,0 +1,13 @@
+#' @title Find optimal lambda
+#' @description Uses lambda-means algorithm to find the optimal penalty
+#'   parameter for use with [dp_means].
+#' @param x numeric matrix or data frame of features
+#' @references Comiter, M. Z., Cha, M., Kung, H. T., & Teerapittayanon, S.
+#'   (2016). Lambda means clustering: automatic parameter search and
+#'   distributed computing implementation. *2016 23rd International Conference
+#'   on Pattern Recognition (ICPR)*, 2331–2337. DOI:
+#'   [10.1109/ICPR.2016.7899984](https://doi.org/10.1109/icpr.2016.7899984)
+#' @export
+lambda_means <- function(x) {
+  # TODO
+}
